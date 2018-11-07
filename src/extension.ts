d0f8550b261785ec1cf9ca4b83c70f60eb35105f
Grigory Moshkevich <mgm@nonamelab.ru>
2018-11-07 16:57:08 +0300

    ngf

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,20 @@
 import * as vscode from 'vscode';
 
 var outputChannel = vscode.window.createOutputChannel('asd')
+let b = '#6adb36'
 
-function _toTwoDigitHex(n: number): string {
+function toHex(n: number): string {
   const r = n.toString(16);
-  outputChannel.appendLine('yo: 1:' + ('0' + r) + ' - 2: ' + r + '   ' + (r.length !== 2 ? '0' + r : r));
   return r.length !== 2 ? '0' + r : r;
+}
+
+function toRgba(n: string): vscode.Color {
+  n = n.replace('#', '')
+  const r = parseInt(n.slice(0,2), 16) / 255
+  const g = parseInt(n.slice(2,4), 16) / 255
+  const b = parseInt(n.slice(4,6), 16) / 255
+  const a = n.length > 7 ? parseInt(n.slice(4,6), 16) / 255 : 1
+  return new vscode.Color(r, g, b, a) // get colors from whats there in text now
 }
 
 const GO_MODE: vscode.DocumentFilter = { language: 'javascript', scheme: 'file' };
@@ -14,20 +23,22 @@ class GoColorProvider implements vscode.DocumentColorProvider {
   public provideDocumentColors(
     document: vscode.TextDocument, token: vscode.CancellationToken):
     Thenable<vscode.ColorInformation[]> {
+      // outputChannel.appendLine(JSON.stringify(toRgba(b)));
       return new Promise(resolve => {
         resolve([{
-          range: new vscode.Range(0, 0, 0, 20),
-          color: new vscode.Color(0,0,0,1) // get colors from whats there in text now
+          range: new vscode.Range(0, 28, 0, 28 + b.length),
+          color: toRgba(b)
         }])
       })
   }
   public provideColorPresentations(
     color: vscode.Color, context: { document: vscode.TextDocument, range: vscode.Range }, token: vscode.CancellationToken):
     Thenable<vscode.ColorPresentation[]> {
+      outputChannel.appendLine('2  ' + color.red + ' ' + color.green + ' ' + color.blue);
+      b = `#${toHex(color.red * 255)}${toHex(color.green * 255)}${toHex(color.blue * 255)}${color.alpha !== 1 ? toHex(Math.round(color.alpha * 255)) : ''}`
       return new Promise(resolve => {
-        outputChannel.appendLine('bro' + color.alpha);
         resolve([{
-          label: `#${_toTwoDigitHex(color.red * 255)}${_toTwoDigitHex(color.green * 255)}${_toTwoDigitHex(color.blue * 255)}${color.alpha !== 1 ? _toTwoDigitHex(Math.round(color.alpha * 255)) : ''}`
+          label: b
         }])
       }) 
   }
