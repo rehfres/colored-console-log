382b6d0feef8b37547e5a6c50694a111a0866e02
Grigory Moshkevich <mgm@nonamelab.ru>
2018-11-08 00:33:56 +0300

    kind of finished already. i mean can usable yo

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,19 @@
 import * as vscode from 'vscode';
 
-var outputChannel = vscode.window.createOutputChannel('asd')
-let b = '#6adb36'
-var selectionRange: vscode.Range
-var selectedText: string
-var a = 0
-var m
+const activeEditor = vscode.window.activeTextEditor
+const document = activeEditor.document
+let b = ['#6adb36']
 
 function toHex(n: number): string {
   const r = n.toString(16);
   return r.length !== 2 ? '0' + r : r;
+}
+
+let newColorIndex = 0
+function newColor() {
+  const colors = ['#916e9b', '#d19de0', '#ebb5d5', '#f6c6b4', '#f7d09c', '#dfbead']
+  newColorIndex = (newColorIndex + 1) % 6
+  return colors[newColorIndex]
 }
 
 function toRgba(n: string): vscode.Color {
@@ -27,26 +31,18 @@ class GoColorProvider implements vscode.DocumentColorProvider {
   public provideDocumentColors(
     document: vscode.TextDocument, token: vscode.CancellationToken):
     Thenable<vscode.ColorInformation[]> {
-      // outputChannel.appendLine(JSON.stringify(toRgba(b)));
       let temp = []
-      // let ss = [{
-      //   range: new vscode.Range(0, 28, 0, 28 + b.length),
-      //   color: toRgba(b)
-      // }]
       let m
-      let text = vscode.window.activeTextEditor.document.getText()
-      let pattern = /console.log\('%c%s', 'color: #([a-f0-9]{6}(?:[a-f0-9]{0,2}))\b/g
+      let text = document.getText()
+      let pattern = /console.log\('%c%s', 'color: (#([a-f0-9]{6}(?:[a-f0-9]{0,2}))\b)/g
+      b = []
       while (m = pattern.exec(text)) {
-        const positionStart: vscode.Position = vscode.window.activeTextEditor.document.positionAt(pattern.lastIndex)
-        const positionEnd: vscode.Position = positionStart.translate(0, b.length)
-        console.log(positionStart, positionEnd)
-        temp.push({
-          range: new vscode.Range(positionStart, positionEnd),
-          color: toRgba(b)
-        })
-        console.log(temp)
+        b.push(m[1])
+        const positionStart = document.positionAt(pattern.lastIndex - m[1].length)
+        const positionEnd = positionStart.translate(0, m[1].length)
+        temp.push(new vscode.ColorInformation(new vscode.Range(positionStart, positionEnd), toRgba(b[b.length - 1])))
       }
-      console.log(temp)
+      console.log(b)
       return new Promise(resolve => {
         resolve(temp)
       })
@@ -54,35 +50,29 @@ class GoColorProvider implements vscode.DocumentColorProvider {
   public provideColorPresentations(
     color: vscode.Color, context: { document: vscode.TextDocument, range: vscode.Range }, token: vscode.CancellationToken):
     Thenable<vscode.ColorPresentation[]> {
-      outputChannel.appendLine('2  ' + color.red + ' ' + color.green + ' ' + color.blue);
-      b = `#${toHex(color.red * 255)}${toHex(color.green * 255)}${toHex(color.blue * 255)}${color.alpha !== 1 ? toHex(Math.round(color.alpha * 255)) : ''}`
-      return new Promise(resolve => {
-        resolve([{
-          label: b
-        }])
-      }) 
+        return new Promise(resolve => {
+        resolve(b.map(el => {
+          const bb = `#${toHex(color.red * 255)}${toHex(color.green * 255)}${toHex(color.blue * 255)}${color.alpha !== 1 ? toHex(Math.round(color.alpha * 255)) : ''}`
+          return {label: bb}
+        }))
+      })
   }
 }
 
 export function activate(ctx: vscode.ExtensionContext): void {
-  // console.log('yo')
-  // console.log(/console.log\('%c%s', 'color: /g.exec(vscode.window.activeTextEditor.document.getText()))
   vscode.window.onDidChangeTextEditorSelection(event => {
-    if (vscode.window.activeTextEditor.selection.isEmpty) return
-    a++
-    // vscode.debug.activeDebugConsole.appendLine(JSON.stringify(vscode.window.activeTextEditor.selection));
-    selectionRange = new vscode.Range(vscode.window.activeTextEditor.selection.start, vscode.window.activeTextEditor.selection.end)
-    selectedText = vscode.window.activeTextEditor.document.getText(selectionRange)
-    // console.log(selectionRange)
-    // console.log(selectedText)
-    // console.log(vscode.window.activeTextEditor.document.lineAt(vscode.window.activeTextEditor.selection.end.line))
-    // if (a >= 7) 
-    vscode.window.activeTextEditor.edit(eb => eb.insert(
-      new vscode.Position(vscode.window.activeTextEditor.selection.end.line, vscode.window.activeTextEditor.document.lineAt(vscode.window.activeTextEditor.selection.end.line).range.end.character)
-      // new vscode.Position(3,0)
-      , 
-      `\nconsole.log('%c%s', 'color: ${b}', ${selectedText});`
-    ))
+    const selection = activeEditor.selection
+    if (selection.isEmpty) return
+    const selectedText = document.getText(selection)
+    const thisLine = document.lineAt(selection.end.line)
+    const nextLine = document.lineAt(selection.end.translate(1,0).line)
+    const whiteSpacesNumber = Math.max(thisLine.firstNonWhitespaceCharacterIndex, nextLine.firstNonWhitespaceCharacterIndex)
+    let spaceee = ' '.repeat(whiteSpacesNumber)
+    console.log(spaceee)
+
+    const endOfThisLine = new vscode.Position(selection.end.line, thisLine.range.end.character)
+    const insertText = `\n${spaceee}console.log('%c%s', 'color: ${newColor()}', ${selectedText});`
+    activeEditor.edit(eb => eb.insert(endOfThisLine, insertText))
   })
   ctx.subscriptions.push(
     vscode.languages.registerColorProvider(
